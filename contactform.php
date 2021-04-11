1345b4e168d45e7adc5af7f24d2adfaaff2b2b31
Amisha Jha <63840354+amishajha@users.noreply.github.com>
2021-04-11 21:14:29 +0530

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,20 +1,20 @@
+
 <?php
-$name=$_POST['name'];
-$phone=$_POST['phone'];
-$visitor_email=$_POST['email'];
-$message=$_POST['message'];
-
-
-$email_from='https://amishajha.github.io/';
-$email_subject="New Form Submission";
-$email_body="User Name: $name.\n".
-             "User Email:$visitor_email.\n."
-             "User Phone:$phone.\n."
-             "User Message:$message.\n";
-$to="jhaamisha096@gmail.com";
-$headers="From: $email_from\r\n";
-$headers="Reply-To:$visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
-header("Location:index.html");
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+$message= $_POST['message'];
+$to = "jhaamisha096@mail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message."\r\nPhone num=".$phone;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
 
 ?>
